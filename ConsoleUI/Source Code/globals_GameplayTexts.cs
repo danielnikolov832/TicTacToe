5ae1e86d3afad2b40bf089222da5e00a6dbbd006
Daniel Nikolov <danielsnikolov@gmail.com>
2022-01-23 12:06:49 +0200

    Moved code without global usage to where its used

--- ConsoleUI/Source Code/globals_GameplayTexts.cs
+++ ConsoleUI/Source Code/globals_GameplayTexts.cs
@@ -4,18 +4,9 @@ using TicTacToe.GameDataManagement;
 namespace ConsoleUI
 {
     // Stores globally used text for the gameplay
-    public static class globals_GameplayTexts
+    internal static class globals_GameplayTexts
     {
-        public const string introductoryText = "Hi, ready for some good old TicTacToe";
-        public const string winText = "We won";
-
-        public const string pressAnyKeyToContinueText = "Press any key to continue";
-        public const string pressAnyKeyToExitText = "Press any key to exit";
-
-        public const string selectRowText = "Please enter a number between 1 and 3 to select a corresponding row";
-        public const string selectColumnText = "Please enter a number between 1 and 3 to select a corresponding column";
-
-        public const string invalidInputText = "Invalid input";
-        public const string unsuccessfulAssignmentText = "Element was not assigned successfully, try different coordinates";
+        internal const string pressAnyKeyToContinueText = "Press any key to continue";
+        internal const string pressAnyKeyToExitText = "Press any key to exit";
     }
 }
